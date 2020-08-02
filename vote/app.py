ff3f59df754ab9f5a286ff2a103715599dd47cc5
godomainz <34884192+godomainz@users.noreply.github.com>
2020-08-02 13:50:25 +0530

    added redis_password

--- vote/app.py
+++ vote/app.py
@@ -8,12 +8,13 @@ import json
 option_a = os.getenv('OPTION_A', "Cats")
 option_b = os.getenv('OPTION_B', "Dogs")
 hostname = socket.gethostname()
+redis_password = os.getenv('REDIS_PASSWORD', "Dogs")
 
 app = Flask(__name__)
 
 def get_redis():
     if not hasattr(g, 'redis'):
-        g.redis = Redis(host="redis", db=0, socket_timeout=5)
+        g.redis = Redis(host="redis", db=0, password=redis_password, socket_timeout=5)
     return g.redis
 
 @app.route("/", methods=['POST','GET'])
